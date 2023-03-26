9de3611d5d78641f2712ab4660b7bd709cf21f49
Matheus Oliveira <matheusfilho98.ms@gmail.com>
2023-03-26 17:57:35 -0300

    feat: learn serveless deploy

---
+++ serverless-app/terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  type        = string
+  description = ""
+  default     = "us-east-1"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = ""
+  default     = "learn-terraform"
+}
+
+variable "aws_account_id" {
+  type        = number
+  description = ""
+  default     = 148198459315
+}
+
+variable "service_name" {
+  type        = string
+  description = ""
+  default     = "learn-terraform"
+}
+
+variable "service_domain" {
+  type        = string
+  description = ""
+  default     = "learn-terraform"
+}
