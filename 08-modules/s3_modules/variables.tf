2d23bba1ab4c22130b2d787d912b282ea1e60507
Matheus Oliveira <matheusfilho98.ms@gmail.com>
2023-03-25 23:30:16 -0300

    feat: learn modules

---
+++ 08-modules/s3_modules/variables.tf
@@ -0,0 +1,46 @@
+variable "name" {
+  type        = string
+  description = "Bucket name"
+}
+
+variable "acl" {
+  type        = string
+  description = ""
+  default     = "private"
+}
+
+variable "policy" {
+  type        = string
+  description = ""
+  default     = null
+}
+
+variable "tags" {
+  type        = map(string)
+  description = ""
+  default     = {}
+}
+
+variable "website" {
+  description = "Map containing website configuration."
+  type        = map(string)
+  default     = {}
+}
+
+variable "versioning" {
+  description = "Map containing versioning configuration."
+  type        = map(string)
+  default     = {}
+}
+
+variable "key_prefix" {
+  type        = string
+  description = ""
+  default     = ""
+}
+
+variable "files" {
+  type        = string
+  description = ""
+  default     = ""
+}
